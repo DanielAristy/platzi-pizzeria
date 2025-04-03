790002a61fcbe4479f3fb09c6e54d573b7f6a552
daniel.aristizabal <daniel.aristizabal@sofka.com.co>
2025-04-03 11:49:04 -0500

    Se realiza configuracion de Cors para realizar peticiones de otros origenes

---
+++ src/main/java/com/platzi_pizzeria/web/controller/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.platzi_pizzeria.web.controller.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CorsConfig {
+
+    @Bean
+    CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://example.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
