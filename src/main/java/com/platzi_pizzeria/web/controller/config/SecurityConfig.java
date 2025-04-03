23e7e06df6cfc977b042d2409a1cfdc276f9ee78
daniel.aristizabal <daniel.aristizabal@sofka.com.co>
2025-04-03 11:30:02 -0500

    Configuration basica para la autenticacion por medio de Beans

---
+++ src/main/java/com/platzi_pizzeria/web/controller/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.platzi_pizzeria.web.controller.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@org.springframework.context.annotation.Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        return security
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @
+}
