c7c31654c59c0d0c685431bdf80646c349643828
daniel.aristizabal <daniel.aristizabal@sofka.com.co>
2025-04-04 08:35:19 -0500

    Se crear los roles de ADMIN y CUSTOMER a nivel de Spring Security para darle permisos dentro de nuestra API

--- src/main/java/com/platzi_pizzeria/web/controller/config/SecurityConfig.java
+++ src/main/java/com/platzi_pizzeria/web/controller/config/SecurityConfig.java
@@ -1,10 +1,17 @@
 package com.platzi_pizzeria.web.controller.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @org.springframework.context.annotation.Configuration
@@ -15,7 +22,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
         return security
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
+                        .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
+                        .requestMatchers(HttpMethod.PUT, "/api/pizzas/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/pizzas/**").hasRole("ADMIN")
                         .anyRequest()
                         .authenticated()
                 )
@@ -23,4 +35,26 @@ public class SecurityConfig {
                 .build();
     }
 
+    @Bean
+    public UserDetailsService memoryUsers() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails customer = User.builder()
+                .username("customer")
+                .password(encoder().encode("customer"))
+                .roles("CUSTOMER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+
+    @Bean
+    PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+
+    }
 }
