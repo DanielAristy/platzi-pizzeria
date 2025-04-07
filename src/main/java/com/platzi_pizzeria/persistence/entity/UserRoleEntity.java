fbd494e02d6f63bfa9e970bd8acf2a013ca07ec3
daniel.aristizabal <daniel.aristizabal@sofka.com.co>
2025-04-07 09:37:12 -0500

    Se realiza manejo de roles, utlizando la base de datos para extraerlos

---
+++ src/main/java/com/platzi_pizzeria/persistence/entity/UserRoleEntity.java
@@ -0,0 +1,31 @@
+package com.platzi_pizzeria.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_role")
+@IdClass(RoleId.class)
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserRoleEntity {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Id
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Column(name = "granted_date", nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime grantedDate;
+
+    @ManyToOne
+    @JoinColumn(name = "username", referencedColumnName = "username", insertable = false)
+    private UserEntity user;
+}
--- src/main/java/com/platzi_pizzeria/service/UserSecurityService.java
+++ src/main/java/com/platzi_pizzeria/service/UserSecurityService.java
@@ -1,6 +1,7 @@
 package com.platzi_pizzeria.service;
 
 import com.platzi_pizzeria.persistence.entity.UserEntity;
+import com.platzi_pizzeria.persistence.entity.UserRoleEntity;
 import com.platzi_pizzeria.persistence.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
@@ -23,10 +24,12 @@ public class UserSecurityService implements UserDetailsService {
         UserEntity userEntity = this.userRepository.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found."));
 
+        String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
+
         return User.builder()
                 .username(userEntity.getUsername())
                 .password(userEntity.getPassword())
-                .roles("ADMIN")
+                .roles(roles)
                 .accountLocked(userEntity.getLocked())
                 .disabled(userEntity.getDisabled())
                 .build();
