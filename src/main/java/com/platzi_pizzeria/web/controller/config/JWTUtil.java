a2ba80b3c3a3de2c63d9498cfacb5618c2d845a3
daniel.aristizabal <daniel.aristizabal@sofka.com.co>
2025-05-09 14:53:02 -0500

    Creando utilidad de JWT para genera un token con fecha de expiracion

---
+++ src/main/java/com/platzi_pizzeria/web/controller/config/JWTUtil.java
@@ -0,0 +1,23 @@
+package com.platzi_pizzeria.web.controller.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class JWTUtil {
+
+    private static String SECRET_KEY = "pl4tzi_p1zz3r4";
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+
+    public String create(String username) {
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("platzi_pizzeria")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}
